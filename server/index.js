49538c733d5d7a595e65f823724e367308ef44c4
Hassane Bendaoud <40140987+realHassaneBen@users.noreply.github.com>
2022-06-10 18:25:09 +0100

    fix connection db

--- server/index.js
+++ server/index.js
@@ -17,15 +17,10 @@ const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(CONNECTION_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(CONNECTION_URL)
     .then(() =>
         app.listen(PORT, () =>
             console.log(`Server Running on Port: http://localhost:${PORT}`)
         )
     )
     .catch((error) => console.log(`${error} did not connect`));
-
-mongoose.set('useFindAndModify', false);
